a55150d1f505a80206ad6ee14052367302c05074
Sean McLane <sean@seanmclane.com>
2021-02-21 17:53:57 -0700

    fix address

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -16,7 +16,7 @@ function Contact(props: Props): ReactElement {
     <section className="mx-auto px-6 my-2 lg:m-4 text-center">
       <h1 className="text-2xl mb-2">Mesa Overland</h1>
       <p className="text-lg">1020 Old 6 and 50</p>
-      <p className="text-lg">Mesa, CO 81525</p>
+      <p className="text-lg">Mack, CO 81525</p>
       <p className="text-lg">{props.data.site.siteMetadata.contact_email}</p>
     </section>
   )
