cca42b712403882b098562ba4d179f021534baa8
seanmclane <seanmclane@users.noreply.github.com>
2021-12-27 15:13:36 -0500

    update hero to have image or video (#27)
    * update hero to have image or video
    
    * plus youtube link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,17 +6,34 @@ interface Props {
   details?: string
   image?: any
   imageAlt?: string
+  videoSrc?: string
   className?: string
 }
 
 function Hero(props: Props): ReactElement {
   return (
     <section className={props.className}>
-      <Img
-        fluid={props.image}
-        alt={props.imageAlt}
-        className="mx-auto max-w-5xl"
-      />
+      {props.videoSrc ? (
+        <div
+          style={{ paddingTop: "56.25%" }}
+          className="overflow-hidden relative mb-1"
+        >
+          <iframe
+            className="border-0 h-full w-full left-0 top-0 absolute"
+            src={props.videoSrc}
+            title="Video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen={false}
+            frameBorder="0"
+          ></iframe>
+        </div>
+      ) : (
+        <Img
+          fluid={props.image}
+          alt={props.imageAlt}
+          className="mx-auto max-w-5xl"
+        />
+      )}
       <div className="flex-col text-center py-8 px-2">
         <h1 className="font-title uppercase text-5xl font-bold">
           {props.tagline}
